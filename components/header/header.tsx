2b5a7a20826ae31da2ec7262528f1fc4b7c60cf0
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2026-07-30 12:11:02 -0300

    Criando o Header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,7 +1,16 @@
+import { Logo } from "../logo"
+import { ThemeToggle } from "../theme-toggle"
+
 export const Header = () => {
 	return (
-		<header>
-			...
+		<header className="flex justify-between items-center my-5 mx-3">
+			<div className="flex items-center gap-3">
+				<Logo />
+				<ThemeToggle />
+			</div>
+			<div className="">
+				...
+			</div>
 		</header>
 	)
 }
