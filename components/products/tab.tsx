5990dcf79f6e4e91a7aeb76564c8ee419d610059
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2026-08-20 13:19:37 -0300

    Criando Tab de Produtos 1

---
+++ components/products/tab.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+export const ProductsTab = () => {
+	return (
+		<Tabs defaultValue="tab1">
+			<TabsList className="w-full flex">
+				<TabsTrigger value="tab1" className="flex-1">Tab 1</TabsTrigger>
+				<TabsTrigger value="tab2" className="flex-1">Tab 2</TabsTrigger>
+			</TabsList>
+			<TabsContent value="tab1">
+				Content da TAB 1
+			</TabsContent>
+			<TabsContent value="tab2">
+				Content da TAB 2
+			</TabsContent>
+		</Tabs>
+	);
+}
