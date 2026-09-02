4a7a1e3c8ee9d2a10f9e50e93cf9139aac5dc4f7
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2026-09-02 16:04:58 -0300

    Listando os Produtos 3

---
+++ components/products/item.tsx
@@ -0,0 +1,13 @@
+import { Product } from "@/src/types/product";
+
+type Props= {
+	item: Product;
+}
+
+export const ProductItem = ({ item }: Props) => {
+	return (
+		<div>
+			...
+		</div>
+	);
+}
--- components/products/tab.tsx
+++ components/products/tab.tsx
@@ -1,6 +1,8 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { getAllProducts } from "@/src/services/product";
 import { Product } from "@/src/types/product";
+import { ProductEmpty } from "./empty";
+import { ProductItem } from "./item";
 
 type Tab = {
 	title: string;
@@ -15,22 +17,22 @@ export const ProductsTab = async () => {
 		{
 			title: 'Sushi',
 			value: 'sushi',
-			products: []
+			products: products.filter(item => item.category === 'sushi')
 		},
 		{
 			title: 'Temaki',
 			value: 'temaki',
-			products: []
+			products: products.filter(item => item.category === 'temaki')
 		},
 		{
 			title: 'Combinados',
 			value: 'pack',
-			products: []
+			products: products.filter(item => item.category === 'pack')
 		},
 		{
 			title: 'Bebidas',
 			value: 'beverage',
-			products: []
+			products: products.filter(item => item.category === 'beverage')
 		},
 	];
 
@@ -48,8 +50,15 @@ export const ProductsTab = async () => {
 				))}
 			</TabsList>
 			{tabs.map(item => (
-				<TabsContent value={item.value} className="mt-6">
-					...
+				<TabsContent value={item.value} className="mt-6" key={item.value}>
+					{item.products.length > 0 &&
+						<div className="grid gap-5 grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
+							{item.products.map(product => (
+								<ProductItem key={product.id} item={product} />
+							))}
+						</div>
+					}
+					{item.products.length === 0 && <ProductEmpty />}
 				</TabsContent>
 			))}
 		</Tabs>
