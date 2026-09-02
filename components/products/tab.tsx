aefa96ff541a8653cb993e4355664f9b87772073
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2026-09-02 14:32:09 -0300

    criando tab de produtos 2

--- components/products/tab.tsx
+++ components/products/tab.tsx
@@ -9,10 +9,10 @@ export const ProductsTab = () => {
 				<TabsTrigger value="tab1" className="flex-1">Tab 1</TabsTrigger>
 				<TabsTrigger value="tab2" className="flex-1">Tab 2</TabsTrigger>
 			</TabsList>
-			<TabsContent value="tab1">
+			<TabsContent value="tab1" className="mt-6">
 				Content da TAB 1
 			</TabsContent>
-			<TabsContent value="tab2">
+			<TabsContent value="tab2" className="mt-6">
 				Content da TAB 2
 			</TabsContent>
 		</Tabs>
