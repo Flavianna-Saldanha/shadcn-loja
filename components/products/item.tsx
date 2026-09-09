869672d81945522c18dc5184eb8fb54fffe1a475
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2026-09-09 10:23:48 -0300

    Criando o Store

--- components/products/item.tsx
+++ components/products/item.tsx
@@ -4,6 +4,7 @@ import { Product } from "@/src/types/product";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { toast } from "@/components/ui/toast";
+import { useCartStore } from "@/src/stores/cart-store";
 
 type Props = {
 	item: Product;
@@ -11,8 +12,10 @@ type Props = {
 }
 
 export const ProductItem = ({ item, priority = false }: Props) => {
+	const { upsertCartItem } = useCartStore(state => state);
 
 	const handleAddButton = () => {
+		upsertCartItem(item, 1);
 		toast.add({
 			title: "Item adicionado ao carrinho",
 			description: `${item.name} foi adicionado ao seu carrinho.`,
