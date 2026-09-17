34e6b17677c7cbc63a18872223978acc8da72b1d
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2026-09-17 11:28:20 -0300

    Enviando pro WhatsApp

--- components/checkout/step-finish.tsx
+++ components/checkout/step-finish.tsx
@@ -1,7 +1,20 @@
+import { useCheckoutStore } from "@/src/stores/checkout-store";
+import { Button } from "../ui/button";
+import Link from "next/link";
+
 export const StepFinish = () => {
+	const { name } = useCheckoutStore(state => state)
+
+	const message = 'Oi, tudo bem?';
+	const linkZap = `https://wa.me//${process.env.NEXT_PUBLIC_ZAP}?text=${encodeURI(message)}`
+
 	return (
-		<div>
-			...
+		<div className="text-center flex flex-col gap-5">
+			<p>Perfeito <strong>{name}</strong>!</p>
+			<p>Agora envie seu pedido ao nosso WhatsApp para concluir. Nosso atendente irá te guiar sobre o andamento do pedido.</p>
+			<Button>
+				<Link target="_blank" href={linkZap}>Enviar para o WhatsApp</Link>
+			</Button>
 		</div>
 	);
 }
