85887aab9bae18fdce37886891e21dab62210690
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2026-09-17 11:47:01 -0300

    Montando mensagem

---
+++ lib/generate-message.ts
@@ -0,0 +1,21 @@
+import { useCartStore } from "@/src/stores/cart-store";
+import { useCheckoutStore } from "@/src/stores/checkout-store";
+
+export const generateMessage = () => {
+	const { name, address } = useCheckoutStore.getState();
+	const { cart } = useCartStore.getState();
+
+	const orderProducts = [];
+
+	for (const item of cart) {
+		orderProducts.push(`${item.quantity}x ${item.product.name}`);
+	}
+
+	return `**Dados do cliente:**
+Nome: ${name}
+Endereço: ${address.street}, ${address.number}, (${address.complement})
+${address.district}, ${address.city}/${address.state}
+------
+**Pedido:**
+${orderProducts.join("\n")}`;
+};
